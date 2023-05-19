ee67cc529f4919d5642c04f7c584a5b5075d38cb
Petr <pilot_master_68@mail.ru>
2023-05-19 13:34:31 +0300

    klava1

---
+++ app/src/main/java/com/example/customkeyboard1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.customkeyboard1
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_keyboard)
+
+        @Suppress("DEPRECATION")
+//Мы должны унаследоваться от InputMethodService() и применить слушатель KeyboardView.OnKeyboardActionListener и
+//переопределить нужные нам методы
+        class InputMethodServiceImpl : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+            //В этом методе мы привязываем все наши верстки созданные ранее и привязываем слушатель
+            override fun onCreateInputView(): View {
+                val keyboardView = layoutInflater.inflate(R.layout.custom_keyboard, null) as KeyboardView
+                val keyboard = Keyboard(this, R.xml.num_pad)
+                keyboardView.keyboard = keyboard
+                keyboardView.setOnKeyboardActionListener(this)
+
+                return keyboardView
+            }
+            override fun swipeRight() {
+            }
+            override fun onPress(p0: Int) {
+            }
+            override fun onRelease(p0: Int) {
+            }
+            override fun swipeLeft() {
+            }
+            override fun swipeUp() {
+            }
+            override fun swipeDown() {
+            }
+            //Сейчас нам нужно переопределить только этот метод
+            override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+                //Вызываем наше соединение с сервисом ввода
+                //И обрабатываем нажатия
+                currentInputConnection?.commitText(primaryCode.toString(), 1)
+            }
+            override fun onText(p0: CharSequence?) {
+            }
+        }
+    }
+}
